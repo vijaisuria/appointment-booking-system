af817cd6da74bbdb582337d5f1c5e85c20e192d4
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-01-01 16:26:48 +0530

    Update title in home.php

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['registerNumber'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User Dashboard</title>
+  <title>User Dashboard | MIT-HC</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
     integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
